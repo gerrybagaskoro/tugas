5956079b2102356dcf5641dccf8160e6c7cbb60c
Gerry Bagaskoro Putro <my.gerry139@gmail.com>
2025-07-31 11:21:05 +0700

    tugas1flutter

---
+++ ppkd-b3/ppkd_b_3/lib/start.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+// stl
+class StartWidget extends StatelessWidget {
+  const StartWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Appbar
+      appBar: AppBar(
+        title: Text("Profil Saya"),
+        backgroundColor: Colors.teal,
+        actions: [Icon(Icons.favorite), Icon(Icons.message), Icon(Icons.settings)],
+      ),
+      // Body
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Nama: Gerry Bagaskoro Putro",
+            style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic, fontFamily: "firasans"),
+          ),
+          Row(children: [Icon(Icons.location_on), Text("Jakarta Pusat")]),
+          Text("Seorang pelajar yang sedang belajar Flutter."),
+          Text("Sedang menempuh pelatihan di PPKD Jakarta Pusat"),
+        ],
+      ),
+    );
+  }
+}
